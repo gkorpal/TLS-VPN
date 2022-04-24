f4c396a29cffdd67194608d536834a1dc26da424
gkorpal <8099449+gkorpal@users.noreply.github.com>
2022-04-24 16:47:22 -0700

    Update test.sh

--- practice/PKI-TLS/test.sh
+++ practice/PKI-TLS/test.sh
@@ -6,4 +6,5 @@
 #terminal 2
 ./client.py SEEDPKILab2020.com
 
-# We use SEEDPKILab.com for the host name parameter when running the code on the client, so we need to configure the static IP in the local /etc/hosts file and add the  127.0.0.1 SEEDPKILab2020.com entry so that the client will access it locally.
+# We use SEEDPKILab.com for the host name parameter when running the code on the client, so we need to configure the static IP 
+# in the local /etc/hosts file and add the  127.0.0.1 SEEDPKILab2020.com entry so that the client will access it locally.
