0a0cca94c875d8c8f1f74a06b6473c1ef8e21fba
gkorpal <8099449+gkorpal@users.noreply.github.com>
2022-05-12 16:10:53 -0700

    Create task4.php

---
+++ practice/SQL_Injection_Attack/task4.php
@@ -0,0 +1,36 @@
+<?php
+// Function to create a sql connection.
+function getDB() {
+  $dbhost="10.9.0.6";
+  $dbuser="seed";
+  $dbpass="dees";
+  $dbname="sqllab_users";
+
+  // Create a DB connection
+  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error . "\n");
+  }
+  return $conn;
+}
+
+$input_uname = $_GET['username'];
+$input_pwd = $_GET['Password'];
+$hashed_pwd = sha1($input_pwd);
+
+// create a connection
+$conn = getDB();
+
+// do the query
+$stmt = $conn->prepare("SELECT id, name, eid, salary, ssn
+   	                FROM credential
+                        WHERE name= ? and Password=?");
+// Bind parameters to the query
+$stmt->bind_param("ss", $input_uname, $hashed_pwd);
+$stmt->execute();
+$stmt->bind_result($id, $name, $eid, $salary, $ssn);
+$stmt->fetch();
+
+// close the sql connection
+$conn->close();
+?>
